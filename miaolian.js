daa1158226f6cde9cda2129e597a1b8eb9d24e72
yaheex <40331789+yaheex@users.noreply.github.com>
2023-12-26 14:19:15 +0800

    Update miaolian.js

--- miaolian.js
+++ miaolian.js
@@ -10,7 +10,6 @@ var body = $response.body;
 var obj = JSON.parse(body); 
 // 修改数据字段
 obj.data.level = "3";
-obj.data.expireTime = "6666666666666";
-obj.data.freeTime = "666666666";
+obj.data.agentNum = "5";
 // 重写响应体
 $done({ body: JSON.stringify(obj) });
