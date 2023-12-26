a12733da8a23e45c8ff1e1dd5854c0c1db8cf6c9
yaheex <40331789+yaheex@users.noreply.github.com>
2023-12-26 14:01:21 +0800

    Update miaolian.js

--- miaolian.js
+++ miaolian.js
@@ -9,7 +9,7 @@ hostname=*.mlinkapp.cc
 var body = $response.body; 
 var obj = JSON.parse(body); 
 // 修改数据字段
-obj。data.username = "雅熙霸主丶";
+obj。data.username = "YaHee, Studio.";
 obj。data.level = "3";
 obj。data.freeTime = "659200000";
 // 重写响应体
