2268bba10f7fca80553759873da47be34daedaa7
yaheex <40331789+yaheex@users.noreply.github.com>
2024-02-24 16:47:41 +0800

    Create kssmw.js

---
+++ kssmw.js
@@ -0,0 +1,15 @@
+/*
+夸克扫描王：https://apps.apple.com/app/id1160172628
+夸克浏览器搜索：夸克扫描王
+
+[rewrite_local]
+^https?:\/\/acs\.m\.taobao\.com\/gw\/mtop\.sm\.order\.service\.member\.getmemberinfo url script-response-body https://raw.githubusercontent.com/yaheex/Qx/main/kssmw.js
+
+[MITM]
+hostname = acs.m.taobao.com
+
+*/
+var yahee = JSON.parse($response.body);
+yahee.data.data.userInfo.memberType = "VIP";
+yahee.data.data.userInfo.vipExpirationTime = "148204937166000";
+$done({ body: JSON.stringify(yahee) });
